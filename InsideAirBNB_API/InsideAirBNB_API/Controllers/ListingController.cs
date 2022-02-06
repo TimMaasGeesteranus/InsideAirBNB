94b05a174a3750e7feb7b6b0ccf64c70552ab53a
Tim Maas Geesteranus <tim__mg@hotmail.com>
2022-02-06 19:56:19 +0100

    add listingcontroller

---
+++ InsideAirBNB_API/InsideAirBNB_API/Controllers/ListingController.cs
@@ -0,0 +1,25 @@
+﻿using InsideAirBNB_API.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsideAirBNB_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListingController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ListingController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetListings()
+        {
+            var listings = _appDbContext.SummaryListings.Take(20);
+
+            return Ok(listings);
+        }
+    }
+}
