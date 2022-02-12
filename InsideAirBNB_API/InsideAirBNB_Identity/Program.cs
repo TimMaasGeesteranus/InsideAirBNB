68cacfc95e792d21d5ad505a5b16dd280a8c0e61
Tim Maas Geesteranus <tim__mg@hotmail.com>
2022-02-12 09:34:12 +0100

    add identity project

---
+++ InsideAirBNB_API/InsideAirBNB_Identity/Program.cs
@@ -0,0 +1,39 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using InsideAirBNB_Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//IdentityServer4
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiResources(Config.ApiResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddTestUsers(Config.Users)
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseIdentityServer();
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
