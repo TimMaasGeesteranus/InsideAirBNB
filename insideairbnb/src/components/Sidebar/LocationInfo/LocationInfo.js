891291e484472c94ed5b61d183a98aa4a08225fa
Tim Maas Geesteranus <tim__mg@hotmail.com>
2022-02-20 08:42:15 +0100

    display data when clicked on marker

--- insideairbnb/src/components/Sidebar/LocationInfo/LocationInfo.js
+++ insideairbnb/src/components/Sidebar/LocationInfo/LocationInfo.js
@@ -1,11 +1,43 @@
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
+import "./LocationInfo.css";
 
 const LocationInfo = (props) => {
 
     return (
-        <div>
-            {props.marker.name}
+        <div className="locationInfoText">
+            <div className="bigText bold blueText">
+                {props.marker.name}
+            </div>
+            <div className="smallText">
+                Hosted by: {props.marker.hostName}
+            </div>
+
+            <hr />
+
+            <div className="bigText">
+                €{props.marker.price} per night
+            </div>
+
+            <br />
+            <div className="mediumText">
+                Minimum amount of nights:
+                <div className="floatRight bold">
+                    {props.marker.minimumNights}
+                </div>
+            </div>
+            <div className="mediumText">
+                Roomtype:
+                <div className="floatRight bold">
+                    {props.marker.roomType}
+                </div>
+            </div>
+            <div className="mediumText">
+                Neighbourhood:
+                <div className="floatRight bold">
+                    {props.marker.neighbourhood}
+                </div>
+            </div>
         </div>
     )
 }
